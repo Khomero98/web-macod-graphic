5d6f198014be3f99481b2d81db2b4adf25f77676
Khomero98 <pulido.juandavid@gmail.com>
2023-10-24 17:00:15 -0500

    Initial commit

---
+++ js/scripting.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('a[href="#tabs-2"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
