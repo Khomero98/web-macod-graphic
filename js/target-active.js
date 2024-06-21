4f033dca0b277ae9728f704b3747f2cbd7da25c0
Khomero98 <pulido.juandavid@gmail.com>
2024-06-21 15:46:17 -0500

    borrado de cls

--- js/target-active.js
+++ js/target-active.js
@@ -1,10 +1,8 @@
 const URLActual = window.location.hash
-console.log(URLActual)
 var selector = 'a[href="' + URLActual + '"] , '+ URLActual
 
   function agregarClase() {
     var elementos = document.querySelectorAll(selector);
-    console.log(elementos)
     if(elementos){
         elementos.forEach(function(elemento) {
             elemento.classList.add("show","active");
